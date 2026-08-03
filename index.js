dc44d56f8f8036bd5f6935584b643d6a295defa2
Caleb Stanfield <finn0010@gmail.com>
2026-08-03 03:53:02 -0500

    Added start button functionality
    Added functionality to the start button, and styling, as well as a JSON file for question and answer data.

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 // DOM elements
 const mainHeader = document.getElementById('main-header');
 const mainContent = document.getElementById('main-content');
+let header;
+let startButton;
+
+// Other variables
+const categoryOptions = [
+        { value: 'select', text: 'Select a category' },
+        { value: 'general', text: 'General Knowledge' },
+        { value: 'science', text: 'Science' },
+        { value: 'history', text: 'History' },
+        { value: 'sports', text: 'Sports' },
+        { value: 'entertainment', text: 'Entertainment' }
+    ]
+
+let categorySelect = document.createElement('select');
+    categorySelect.id = 'category-select';
+    categorySelect.classList.add('category-select');
+    categorySelect.id = 'category-select';
+    categorySelect.name = 'category-select';
+
 
 // Function for inital DOM load
 function loadInitialScreen() {
-    let h1 = document.createElement('h1');
-    h1.textContent = 'Welcome! This is a quiz that will test your general knowledge. Click the button below to start the quiz.';
-    h1.classList.add('main-header');
-    mainHeader.appendChild(h1);
-
-    let startButton = document.createElement('button');
+    header = document.createElement('h1');
+    header.textContent = 'Welcome! This is a quiz that will test your general knowledge. Click the button below to start the quiz.';
+    header.classList.add('main-header');
+    mainHeader.appendChild(header);
+    
+    startButton = document.createElement('button');
     startButton.textContent = 'Start Quiz';
     startButton.classList.add('start-button');
     mainContent.appendChild(startButton);
@@ -19,5 +38,24 @@ document.addEventListener('DOMContentLoaded', loadInitialScreen())
 
 // Event listener for start button
 startButton.addEventListener('click', () => {
-    
+    mainHeader.innerHTML = '';
+    mainHeader.textContent = 'Select your category:'
+    mainContent.innerHTML = '';
+
+    mainContent.appendChild(categorySelect);
+
+    categoryOptions.forEach(option => {
+        const optionElement = document.createElement('option');
+        optionElement.value = option.value;
+        optionElement.textContent = option.text;
+        categorySelect.appendChild(optionElement);
+    });
+
 })
+
+
+function loadQuestions(category) {
+    mainContent.innerHTML = '';
+    mainHeader.innerHTML = '';
+}
+
